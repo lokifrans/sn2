5108a1c55c103d36e073e898a84abcf0e28e47b5
kpa <karnaukhopa@nspk.ru>
2024-04-12 16:45:36 +0300

    client update

--- client/client.go
+++ client/client.go
@@ -61,37 +61,36 @@ func main() {
 
 		fmt.Println(First_name, Second_name, age, city)
 
-		// httpPostUrl := "http://127.0.0.1:8080/user/registre"
-		// log.Println("Http POST JSON URL", httpPostUrl)
+		httpPostUrl := "http://127.0.0.1:8080/user/registre"
+		log.Println("Http POST JSON URL", httpPostUrl)
 
-		// var jsonDate = []byte(`
-		// {
-		// 	"first_name" : "test user",
-		// 	"second_name" : "test user",
-		// 	"age" : 100,
-		// 	"biography" : "test user",
-		// 	"city" : "test user",
-		// 	"password" : "test user"
-		// }`)
+		var jsonDate = []byte({
+			"first_name" : First_name,
+			"second_name" : Second_name,
+			"age" : age,
+			"biography" : "test bio",
+			"city" : city,
+			"password" : "testPassword"
+		})
 
-		// req, err := http.NewRequest("POST", httpPostUrl, bytes.NewBuffer(jsonDate))
-		// req.Header.Set("CContent-Type", "application/json; charset=UTF-8")
-		// if err != nil {
-		// 	log.Println(err)
-		// }
+		req, err := http.NewRequest("POST", httpPostUrl, bytes.NewBuffer(jsonDate))
+		req.Header.Set("CContent-Type", "application/json; charset=UTF-8")
+		if err != nil {
+			log.Println(err)
+		}
 
-		// client := &http.Client{}
-		// res, err := client.Do(req)
-		// if err != nil {
-		// 	log.Println()
-		// 	panic(err)
-		// }
-		// defer res.Body.Close()
+		client := &http.Client{}
+		res, err := client.Do(req)
+		if err != nil {
+			log.Println()
+			panic(err)
+		}
+		defer res.Body.Close()
 
-		// log.Println("\nResponse Status", res.Status)
-		// log.Println("\nResponse Header", res.Header)
-		// body, _ := io.ReadAll(res.Body)
-		// log.Println("\nResponse body", string(body))
+		log.Println("\nResponse Status", res.Status)
+		log.Println("\nResponse Header", res.Header)
+		body, _ := io.ReadAll(res.Body)
+		log.Println("\nResponse body", string(body))
 	}
 
 }
